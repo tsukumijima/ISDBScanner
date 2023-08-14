bfef6ac7dc456611a99576e3382f96f132274908
tsukumi <tsukumijima@users.noreply.github.com>
2023-08-14 20:51:09 +0000

    Update: ワンセグサービスを判別できるように変更

--- TSChannelScanner/analyzer.py
+++ TSChannelScanner/analyzer.py
@@ -12,12 +12,12 @@ from typing import Any
 class TransportStreamInfo(BaseModel):
     physical_channel: str = ''
     physical_channel_number: int = -1
-    physical_channel_slot: int = -1
+    physical_channel_slot: int | None = None
     frequency: int = -1
     transport_stream_id: int = -1
     network_id: int = -1
     network_name: str = ''
-    remote_control_key_id: int = -1
+    remote_control_key_id: int | None = None
     services: list[ServiceInfo] = []
 
 class ServiceInfo(BaseModel):
@@ -25,6 +25,7 @@ class ServiceInfo(BaseModel):
     service_name: str = ''
     service_type: int = -1
     is_free: bool = True
+    is_oneseg: bool = False
 
 
 class TransportStreamAnalyzer(TransportStreamFile):
@@ -37,8 +38,8 @@ class TransportStreamAnalyzer(TransportStreamFile):
     def __init__(self, ts_stream_data: bytearray, chunk_size: int = 10000):
         """
         TransportStreamAnalyzer を初期化する
-        BS / CS110 では、NIT や SDT などの SI (Service Information) の送出周期 (2023/08 時点で最大 10 秒周期) の関係で、
-        10 秒以上の長さを持つ TS ストリームを指定する必要がある
+        BS / CS110 では、NIT や SDT などの SI (Service Information) の送出間隔 (2023/08 時点で最大 10 秒周期) の関係で、
+        10 秒以上の長さを持つ TS ストリームを指定する必要がある (地上波の SI 送出間隔は 1 秒周期)
 
         Args:
             ts_stream_data (bytearray): チューナーから受信した TS ストリーム
@@ -77,24 +78,38 @@ class TransportStreamAnalyzer(TransportStreamFile):
         self.seek(0)
         for nit in self.sections(ActualNetworkNetworkInformationSection):
             for transport_stream in nit.transport_streams:
-                # すでに同じ transport_stream_id の TS が登録されている場合はスキップ
-                if len([ts for ts in ts_info_list.values() if ts.transport_stream_id == transport_stream.transport_stream_id]) > 0:
-                    continue
                 # トランスポートストリームの情報を格納
-                ts_info = TransportStreamInfo()
-                ts_info.transport_stream_id = transport_stream.transport_stream_id
-                ts_info.network_id = nit.network_id
-                ts_info_list[ts_info.transport_stream_id] = ts_info
-                # TSInformationDescriptor は地上波でのみ送出される
-                if ts_info.network_id >= 0x7880 and ts_info.network_id <= 0x7FE8:
-                    for ts_information in transport_stream.descriptors.get(TSInformationDescriptor, []):
-                        ts_info.network_name = ts_information.ts_name_char
-                        ts_info.remote_control_key_id = ts_information.remote_control_key_id
-                        break
-                # 地上波以外では NetworkNameDescriptor の値を使用する
+                # すでに同じ transport_stream_id の TS が登録されている場合は既存の情報を上書きする
+                if transport_stream.transport_stream_id in ts_info_list:
+                    ts_info = ts_info_list[transport_stream.transport_stream_id]
                 else:
+                    ts_info = TransportStreamInfo()
+                    ts_info.transport_stream_id = transport_stream.transport_stream_id
+                    ts_info_list[ts_info.transport_stream_id] = ts_info
+                ts_info.network_id = nit.network_id
+                if ts_info.network_id >= 0x7880 and ts_info.network_id <= 0x7FE8:
+                    # TS 情報記述子 (地上波のみ)
+                    for ts_information in transport_stream.descriptors.get(TSInformationDescriptor, []):
+                        ts_info.network_name = ts_information.ts_name_char  # TS 名 (ネットワーク名として設定)
+                        ts_info.remote_control_key_id = ts_information.remote_control_key_id  # リモコンキー ID
+                        break
+                    # 部分受信記述子 (地上波のみ)
+                    # ワンセグ放送のサービスを特定するために必要
+                    for partial_reception in transport_stream.descriptors.get(PartialReceptionDescriptor, []):
+                        for partial_service in partial_reception.services:
+                            # すでに同じ service_id のサービスが登録されている場合は既存の情報を上書きする
+                            if partial_service.service_id in [sv.service_id for sv in ts_info.services]:
+                                service_info = [sv for sv in ts_info.services if sv.service_id == partial_service.service_id][0]
+                            else:
+                                service_info = ServiceInfo()
+                                service_info.service_id = partial_service.service_id
+                                ts_info.services.append(service_info)
+                            service_info.is_oneseg = True
+                        break
+                else:
+                    # ネットワーク名記述子
                     for network_name in nit.network_descriptors.get(NetworkNameDescriptor, []):
-                        ts_info.network_name = network_name.char
+                        ts_info.network_name = network_name.char  # ネットワーク名 (地上波では "関東広域0" のような値になるので利用しない)
                         break
 
         # SDT からサービスの情報を取得
@@ -105,18 +120,23 @@ class TransportStreamAnalyzer(TransportStreamFile):
                 ts_info = ts_info_list.get(sdt.transport_stream_id)
                 if ts_info is None:
                     continue
-                # すでに同じ service_id のサービスが登録されている場合はスキップ
-                if len([sv for sv in ts_info.services if sv.service_id == service.service_id]) > 0:
-                    continue
                 # サービスの情報を格納
-                service_info = ServiceInfo()
-                service_info.service_id = service.service_id
+                # すでに同じ service_id のサービスが登録されている場合は既存の情報を上書きする
+                if service.service_id in [sv.service_id for sv in ts_info.services]:
+                    service_info = [sv for sv in ts_info.services if sv.service_id == service.service_id][0]
+                else:
+                    service_info = ServiceInfo()
+                    service_info.service_id = service.service_id
+                    ts_info.services.append(service_info)
                 service_info.is_free = not bool(service.free_CA_mode)
-                ts_info.services.append(service_info)
                 for service in service.descriptors.get(ServiceDescriptor, []):
                     service_info.service_type = service.service_type
                     service_info.service_name = service.service_name
                     break
+            # service_id 順にソート
+            ts_info = ts_info_list.get(sdt.transport_stream_id)
+            if ts_info is not None:
+                ts_info.services.sort(key=lambda x: x.service_id)
 
         from devtools import debug
         debug(ts_info_list)
